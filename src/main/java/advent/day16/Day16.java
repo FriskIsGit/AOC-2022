602b63659e8fb0fcc67a5fdc8ac3a659fabc82e9
FriskIsGit <friskisgit@users.noreply.github.com>
2023-01-28 20:47:25 +0100

    part1 improved

--- src/main/java/advent/day16/Day16.java
+++ src/main/java/advent/day16/Day16.java
@@ -2,10 +2,7 @@ package advent.day16;
 
 import advent.AdventOfCode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Day16{
     public static int calls = 0;
@@ -22,15 +19,70 @@ public class Day16{
         List<String> lines = AdventOfCode.readDay(16);
         //List<String> lines = AdventOfCode.readDummy(16);
         List<Valve> allValves = parseValves(lines);
+
+        String start = "AA";
+        Valve startingValve = allValves.stream().filter(v -> v.equals(start)).findFirst().orElse(null);
+        if(startingValve == null){
+            throw new NullPointerException(start + " valve does not exist");
+        }
+        List<Connection> startingConnections = findInitialMajorValves(startingValve);
+
         long st = System.currentTimeMillis();
         List<Valve> majorValves = simplifyGraph(allValves);
         long en = System.currentTimeMillis();
-        System.out.println("Time taken: " + (en-st));
-        System.out.println(majorValves.size());
-        //part1(allValves);
+
+        System.out.println("Time taken to simplify graph: " + (en-st));
+        int majors = majorValves.size();
+        System.out.println("Major valves: " + majors);
+        part1(startingConnections, majors);
     }
 
-    private static List<Valve> simplifyGraph(List<Valve> valves){
+    private static void part1(List<Connection> startingConnections, int majorValves){
+        if(SKIP_UNOPENED){
+            System.out.println("Launched with skips enabled, will take significantly longer to execute");
+        }
+        long st = System.currentTimeMillis();
+
+        int max = 0;
+        for(Connection connection : startingConnections){
+            //minutes are 1-indexed
+            int pressure = explore(connection.valve, connection.travelTime + 1, new boolean[majorValves]);
+            max = Math.max(pressure, max);
+        }
+        System.out.println("Maximum pressure: " + max);
+
+        long en = System.currentTimeMillis();
+        System.out.println("Time taken ms: " + (en-st));
+        System.out.println("Calls to explore: " + calls);
+    }
+
+    //algorithm will be run starting from these valves
+    public static List<Connection> findInitialMajorValves(Valve startingValve){
+        if(startingValve.isMajor){
+            return Collections.singletonList(new Connection(startingValve, 0));
+        }
+        List<Connection> majorValves = new ArrayList<>();
+        for(Connection con : startingValve.connections){
+            findMajorConnections(con.valve, 1, startingValve, majorValves);
+        }
+        return majorValves;
+    }
+
+    private static void findMajorConnections(Valve current, int distance, Valve previous, List<Connection> majorValves){
+        if(current.isMajor){
+            majorValves.add(new Connection(current, distance));
+            return;
+        }
+        for(Connection c : current.connections){
+            //don't go back where you came from
+            if(c.valve == previous){
+                continue;
+            }
+            findMajorConnections(c.valve, distance+1, current, majorValves);
+        }
+    }
+
+    public static List<Valve> simplifyGraph(List<Valve> valves){
         List<Valve> majorValves = new ArrayList<>();
         for(Valve valve : valves){
             if(valve.isMajor){
@@ -55,7 +107,25 @@ public class Day16{
             if(distance == 1){
                 return;
             }
-            dispatchingValve.connections.add(new Connection(current, distance));
+            //check if there's a better connection for this exact valve or any at all
+            List<Connection> connections = dispatchingValve.connections;
+            Iterator<Connection> it = connections.iterator();
+            boolean deleted = false, matchExists = false;
+            while(it.hasNext()){
+                Connection c = it.next();
+                if(c.valve == current){
+                    matchExists = true;
+                    if(c.travelTime > distance){
+                        it.remove();
+                        deleted = true;
+                        break;
+                    }
+                }
+            }
+            if(!matchExists || deleted){
+                connections.add(new Connection(current, distance));
+            }
+
             return;
         }
         for(Connection c : current.connections){
@@ -85,97 +155,51 @@ public class Day16{
         assert(startingValve != null);
         long st = System.currentTimeMillis();
 
-        int maxPressure = findMaximumPressure(startingValve, majorValves);
-        System.out.println("Maximum pressure: " + maxPressure);
+        //int maxPressure = findMaximumPressure(startingValve, majorValves);
+        //System.out.println("Maximum pressure: " + maxPressure);
 
         long en = System.currentTimeMillis();
         System.out.println("Time taken ms: " + (en-st));
         System.out.println("Calls to explore: " + calls);
     }
 
-    //2080-2205 range for input16
-    private static void part1(List<Valve> allValves){
-
-        final String startingValveName = "AA";
-        Valve startingValve = null;
-        int majorValves = 0;
-        for(Valve valve : allValves){
-            if(valve.isMajor){
-                majorValves++;
-            }
-            if(valve.equals(startingValveName)){
-                startingValve = valve;
-            }
-        }
-        System.out.println("majorValves: " + majorValves);
-        assert(startingValve != null);
-        if(SKIP_UNOPENED){
-            System.out.println("Launched with skips enabled, will take significantly longer to execute");
-        }
-        long st = System.currentTimeMillis();
-
-        int maxPressure = findMaximumPressure(startingValve, majorValves);
-        System.out.println("Maximum pressure: " + maxPressure);
-
-        long en = System.currentTimeMillis();
-        System.out.println("Time taken ms: " + (en-st));
-        System.out.println("Calls to explore: " + calls);
-    }
     public static boolean[] copyOf(boolean[] srcArr){
         boolean[] newArr = new boolean[srcArr.length];
         System.arraycopy(srcArr, 0, newArr, 0, srcArr.length);
         return newArr;
     }
 
-    public static int findMaximumPressure(Valve startingValve, int majorValves){
-        //starting node is not a major one and has no origin
-        if(startingValve.flowRate > 0){
-            throw new IllegalStateException("Unexpected flowRate of the starting node");
-        }
-        int max = 0;
 
-//        for(Valve connection : startingValve.valves){
-//            int pressure = explore(connection, 2, startingValve, new boolean[majorValves]);
-//            max = Math.max(pressure, max);
-//        }
-        return max;
-    }
-
-    private static int explore(Valve valve, int minute, Valve origin, boolean[] opened){
+    private static int explore(Valve current, int minute, boolean[] opened){
         calls++;
         if(minute >= Valve.MAX_MINUTES){
             return 0;
             //nothing can be done anymore
         }
-        int valvePressure = valve.getPressure(minute);
+        int valvePressure = current.getPressure(minute);
 
         int maxNodePressure = 0;
-        /*for(Valve con : valve.valves){
-            if(valve.isMajor){
-                //if is opened - always go to connection (previous mistake, not acknowledging the state after the call)
-                boolean[] openedCopy1 = copyOf(opened);
-                if(openedCopy1[valve.index]){
-                    int tempPressure = explore(con, minute+1, valve, openedCopy1);
-                    maxNodePressure = Math.max(tempPressure, maxNodePressure);
-                }
-                else{
-                    //closed but skip (only relevant to dummy input)
-                    if(SKIP_UNOPENED && valve.flowRate < FLOW_RATE_THRESHOLD){
-                        int tempPressure = explore(con, minute+1, valve, copyOf(opened));
-                        maxNodePressure = Math.max(tempPressure, maxNodePressure);
-                    }
-                    //closed - open valve
-                    boolean[] openedCopy2 = copyOf(opened);
-                    openedCopy2[valve.index] = true;
-                    int tempPressure = explore(con, minute+2, valve, openedCopy2) + valvePressure;
-                    maxNodePressure = Math.max(tempPressure, maxNodePressure);
-                }
-            }else if(!con.equals(origin)){
-                // go forward
-                int tempPressure = explore(con, minute+1, valve, copyOf(opened));
+        for(Connection connection : current.connections){
+            Valve con = connection.valve;
+            //if is opened - always go to connection (previous mistake, not acknowledging the state after the call)
+            boolean[] openedCopy1 = copyOf(opened);
+            if(openedCopy1[current.index]){
+                int tempPressure = explore(con, minute + connection.travelTime, openedCopy1);
                 maxNodePressure = Math.max(tempPressure, maxNodePressure);
             }
-        }*/
+            else{
+                //closed but skip (only relevant to dummy input)
+                if(SKIP_UNOPENED && current.flowRate < FLOW_RATE_THRESHOLD){
+                    int tempPressure = explore(con, minute + connection.travelTime, copyOf(opened));
+                    maxNodePressure = Math.max(tempPressure, maxNodePressure);
+                }
+                //closed - open valve
+                boolean[] openedCopy2 = copyOf(opened);
+                openedCopy2[current.index] = true;
+                int tempPressure = explore(con, minute + connection.travelTime + 1, openedCopy2) + valvePressure;
+                maxNodePressure = Math.max(tempPressure, maxNodePressure);
+            }
+        }
         return maxNodePressure;
     }
 
